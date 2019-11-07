be72e6b67e079be569efd8059eb8893992f800bc
Caroline Dafonseca <dafonseca.ca@gmail.com>
2019-11-07 15:43:25 +0100

    delete done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -12,11 +12,15 @@ class DosesController < ApplicationController
     redirect_to cocktail_path(params['cocktail_id'].to_i)
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path
+  end
+
   private
 
   def params_dose
     params.require('dose').permit(:description, :ingredient_id)
   end
-  # def destroy
-  # end
 end
