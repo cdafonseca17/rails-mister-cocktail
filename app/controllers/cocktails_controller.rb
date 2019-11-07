20e91b69483f49495a5d09a0b0d5190827b5f121
Caroline Dafonseca <dafonseca.ca@gmail.com>
2019-11-07 13:03:49 +0100

    Routing, Controller, Views for Cocktails

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,24 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create(params_cocktail)
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def params_cocktail
+    params.require('cocktail').permit(:name)
+  end
+end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,9 +2,8 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :description, presence: true, allow_blank: false
-  validates :cocktail_id, presence: true
-  validates :ingredient_id, presence: true
-  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
-
+  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
+  validates :ingredient, presence: true
+  # validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
   # validates [:cocktail_id, :ingredient_id], uniqueness: true
 end
